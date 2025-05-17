83f0360cb4e4ba24c17cdb2ae5f20ea29f51af22
shimingwei <mingwei.shi@hotmail.com>
2025-05-17 22:13:48 +0800

    version 0.0.1, #2

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "UseePayCore",
-            url: "https://github.com/mingway1991/SMPTest/releases/download/v0.0.1/UseePayCore.xcframework.zip",
+            url: "https://github.com/useepay2020/useepay-ios/releases/download/v0.0.1/UseePayCore.xcframework.zip",
             checksum: "46d186b87f144065f1d67c85eac11193560a23666196b3d2bab6bad2a8944f0a")
     ]
 )
