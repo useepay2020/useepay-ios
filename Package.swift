8c784abc876526ee990646aef7d87d0cadfd7a1e
shimingwei <mingwei.shi@hotmail.com>
2025-05-18 08:49:14 +0800

    version 0.0.1, #5

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
         .binaryTarget(
             name: "UseePayCore",
             url: "https://github.com/useepay2020/useepay-ios/releases/download/0.0.1/UseePayCore.xcframework.zip",
-            checksum: "436cdd213755ba94813eef461a5f011574bb8fbc45bee84592012b9673c76ce4")
+            checksum: "f7ad426de01162211f54edb469ea01c74af2c38795b145f03baf0993d7917cca")
     ]
 )
